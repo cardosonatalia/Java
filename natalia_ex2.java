b483f306664bdb2aba55c5fdffd2b59f16ebc4ea
cardosonatalia <133161096+cardosonatalia@users.noreply.github.com>
2023-05-14 22:39:12 -0300

    Update and rename ex2.java to natalia_ex2.java

--- natalia_ex2.java
+++ natalia_ex2.java
@@ -1,11 +1,12 @@
 /*
-    - Descrição: Exercício 2- 17/04
+    - Descrição: Exercício 2- 17/04 
     - Autor: Natalia 
     - Data: 03/05/23
 */
 import java.util.Scanner;
 public class ex2 {
     public static void main(String args[]){
+       //declarando valor das variaveis
         double n1,n2,n3,n4,res;
         n1=0.0;
         n2=0.0;
@@ -27,7 +28,7 @@ public class ex2 {
          n4=input.nextDouble();
 
          res = (n1*n1)+(n2*n2)+(n3*n3)+(n4*n3);
-      
+      //resultado da conta é a soma dos numeros ao quadrado
         System.out.printf("O resultado é:", +res);
         input.close();
         
